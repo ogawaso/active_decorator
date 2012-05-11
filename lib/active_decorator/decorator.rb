f682448cd5c1418de736765d18f5fcbf3e5ce30d
ogawaso <ogawaso@gmail.com>
2012-05-11 14:19:54 +0900

    fix

--- lib/active_decorator/decorator.rb
+++ lib/active_decorator/decorator.rb
@@ -26,18 +26,21 @@ module ActiveDecorator
           alias_method_chain :to_a, :decorator
         end
       else
+        if obj.respond_to?(:reflections) && !obj.is_a?(ActiveRecord::Relation)
+          belongs_to_column_names = obj.reflections.select{|k, v| v.macro == :belongs_to}.map{|h, v| v.name}
+          belongs_to_column_names.each do |c|
+            belongs_to_obj = obj.send(c)
+            if belongs_to_obj
+              d = decorator_for belongs_to_obj.class
+              next unless d
+              belongs_to_obj.extend d unless belongs_to_obj.is_a? d
+            end
+          end
+        end
+
         d = decorator_for obj.class
         return obj unless d
         obj.extend d unless obj.is_a? d
-        belongs_to_column_names = obj.reflections.select{|k, v| v.macro == :belongs_to}.map{|h, v| v.name}
-        belongs_to_column_names.each do |c|
-          belongs_to_obj = obj.send(c)
-          if belongs_to_obj
-            d = decorator_for belongs_to_obj.class
-            next unless d
-            belongs_to_obj.extend d unless belongs_to_obj.is_a? d
-          end
-        end
       end
     end
 
